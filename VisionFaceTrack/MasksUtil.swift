529eb060cbfd466bb360c021a56bf6db02673d8a
hunble <hunble@arrivy.com>
2019-03-25 05:44:20 +0500

    Added Images resources A Utility file added to support the masks and calculate there adjustment . to keep the logic out of the vision api. Added Mutiple mask layers support to make it scalable

---
+++ VisionFaceTrack/MasksUtil.swift
@@ -0,0 +1,12 @@
+//
+//  masksViewModel.swift
+//  VisionFaceTrack
+//
+//  Created by Muhammad Hunble Dhillon on 3/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+class MasksViewModel{
+    
+}
